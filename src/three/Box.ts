642a812a9cee05d217f0c1347a92518c52f0ba1d
Kenji Saito <ksaito@do-johodai.ac.jp>
2026-01-20 05:28:43 +0900

    寝ぼけている

--- src/three/Box.ts
+++ src/three/Box.ts
@@ -10,7 +10,7 @@ export class Box extends A3Object {
   }
 
   // 引数が上とあってなさそうだけど、こうするのが正解
-  initObject(args: (number | string)) {
+  initObject(args: (number | string)[]) {
     const nums = args.filter((a): a is number => typeof a === "number");
     const strs = args.filter((a): a is string => typeof a === "string");
     const geo = new THREE.BoxGeometry(...nums);
